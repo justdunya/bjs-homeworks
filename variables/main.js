30eba5c1e5c5d4f42a6874603c09dc53900ae21c
justdunya <saucywolf2001@gmail.com>
2019-06-24 14:47:44 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,22 @@
+// Задача 1
+
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let averageMark = (algebra + geography + physics) / 3;
+
+console.log( `Средняя оценка по предметам ${averageMark}`);
+
+// Задача 2
+
+let name = " Татьяна";
+let message = "Привет мир! меня зовут" + name;
+console.log(message);
+
+// Задача 3
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
